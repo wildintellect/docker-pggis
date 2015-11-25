daa1d10d2bf35894c53359b768f047ab3f53db6e
Alex Mandel <aimandel@ucdavis.edu>
2015-11-24 16:34:24 -0800

    Still need to understand how to init the service simply

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,8 @@
 #
 # Version 0.1
 #
+# When testing: docker build -t postgis .
+# Once it's built and on docker hub: ?
 # Originally forked from 
 
 FROM phusion/baseimage
@@ -16,6 +18,7 @@ MAINTAINER Alex Mandel, tech@wildintellect.com
 ENV HOME /root
 #ENV PG_VERSION `apt-cache show postgresql -v |grep Version | awk -F '[: +]' '{print $3}' | head -n 1`
 ENV PG_VERSION 9.3
+ENV POSTGIS_V 2.1
 
 # Regenerate SSH host keys. baseimage-docker does not contain any, so you
 # have to do that yourself. You may also comment out this instruction; the
@@ -33,38 +36,38 @@ RUN apt-add-repository -y ppa:ubuntugis/ubuntugis-unstable
 
 # Update and Install packages
 RUN apt-get update && apt-get install --yes \
+    postgresql \
     postgis \
-    postgresql-$PG_VERSION-postgis-2.1
+    postgresql-$PG_VERSION-postgis-$POSTGIS_V
 
 
 # ---------- SETUP --------------
-
 ## add a baseimage PostgreSQL init script
-#RUN mkdir /etc/service/postgresql
-#ADD postgresql.sh /etc/service/postgresql/run
+RUN mkdir /etc/service/postgresql
+ADD postgresql.sh /etc/service/postgresql/run
 
 ## Adjust PostgreSQL configuration so that remote connections to the
 ## database are possible. 
-#RUN echo "host all  all    0.0.0.0/0  md5" >> /etc/postgresql/9.5/main/pg_hba.conf
+RUN echo "host all  all    0.0.0.0/0  md5" >> /etc/postgresql/$PG_VERSION/main/pg_hba.conf
 
 ## And add ``listen_addresses`` to ``/etc/postgresql/9.5/main/postgresql.conf``
-#RUN echo "listen_addresses='*'" >> /etc/postgresql/9.5/main/postgresql.conf
-
-## Expose PostgreSQL
-#EXPOSE 5432
+RUN echo "listen_addresses='*'" >> /etc/postgresql/$PG_VERSION/main/postgresql.conf
 
 ## Add VOLUMEs to allow backup of config, logs and databases
 #VOLUME  ["/data", "/etc/postgresql", "/var/log/postgresql", "/var/lib/postgresql"]
 
 ## add database setup upon image start
-#ADD pgpass /root/.pgpass
-#RUN chmod 700 /root/.pgpass
-#RUN mkdir -p /etc/my_init.d
-#ADD init_db_script.sh /etc/my_init.d/init_db_script.sh
-#ADD init_db.sh /root/init_db.sh
+ADD pgpass /root/.pgpass
+RUN chmod 700 /root/.pgpass
+RUN mkdir -p /etc/my_init.d
+ADD init_db_script.sh /etc/my_init.d/init_db_script.sh
+ADD init_db.sh /root/init_db.sh
 
 ## ---------- Final cleanup --------------
 ##
 ## Clean up APT when done.
-#RUN apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+RUN apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+## Expose PostgreSQL
+EXPOSE 5432
 
--- init_db.sh
+++ init_db.sh
@@ -18,42 +18,42 @@ echo "PostgreSQL running, initializing database."
 
 /sbin/setuser postgres /usr/bin/psql -c "CREATE USER pggis with SUPERUSER PASSWORD 'pggis';"
 
-# == Auto restore dumps ==
-#
-# If we find some postgresql dumps in /data/restore, then we load it
-# in new databases
-shopt -s nullglob
-for f in /data/restore/*.backup
-do
-	echo "Found database dump to restore : $f"
-    DBNAME=$(basename -s ".backup" "$f")
-    echo "Creating a new database $DBNAME.."
-    /usr/bin/psql -U pggis -h localhost -c "CREATE DATABASE $DBNAME WITH OWNER = pggis     ENCODING = 'UTF8'     TEMPLATE = template0    CONNECTION LIMIT = -1;" postgres
-    /usr/bin/psql -U pggis -h localhost -w -c "CREATE EXTENSION postgis; CREATE EXTENSION postgis_topology; CREATE EXTENSION pgrouting; CREATE EXTENSION pointcloud; CREATE EXTENSION pointcloud_postgis; CREATE EXTENSION postgis_sfcgal; drop type if exists texture; create type texture as (url text,uv float[][]);" $DBNAME
-#    /usr/bin/psql -U pggis -h localhost -w -f /usr/share/postgresql/9.5/contrib/postgis-2.1/sfcgal.sql -d $DBNAME
+## == Auto restore dumps ==
+##
+## If we find some postgresql dumps in /data/restore, then we load it
+## in new databases
+#shopt -s nullglob
+#for f in /data/restore/*.backup
+#do
+#	echo "Found database dump to restore : $f"
+#    DBNAME=$(basename -s ".backup" "$f")
+#    echo "Creating a new database $DBNAME.."
+#    /usr/bin/psql -U pggis -h localhost -c "CREATE DATABASE $DBNAME WITH OWNER = pggis     ENCODING = 'UTF8'     TEMPLATE = template0    CONNECTION LIMIT = -1;" postgres
+#    /usr/bin/psql -U pggis -h localhost -w -c "CREATE EXTENSION postgis; CREATE EXTENSION postgis_topology; CREATE EXTENSION pgrouting; CREATE EXTENSION pointcloud; CREATE EXTENSION pointcloud_postgis; CREATE EXTENSION postgis_sfcgal; drop type if exists texture; create type texture as (url text,uv float[][]);" $DBNAME
+##    /usr/bin/psql -U pggis -h localhost -w -f /usr/share/postgresql/9.5/contrib/postgis-2.1/sfcgal.sql -d $DBNAME
 
-    echo "Restoring database $DBNAME.."
-    /usr/bin/pg_restore -U pggis -h localhost -d $DBNAME -w "$f"
-    echo "Restore done."
-done
+#    echo "Restoring database $DBNAME.."
+#    /usr/bin/pg_restore -U pggis -h localhost -d $DBNAME -w "$f"
+#    echo "Restore done."
+#done
 
-# == Auto restore SQL backups ==
-#
-# If we find some postgresql sql scripts /data/restore, then we load it
-# in new databases
-shopt -s nullglob
-for f in /data/restore/*.sql
-do
-	echo "Found database SQL dump to restore : $f"
-    DBNAME=$(basename -s ".sql" "$f")
-    echo "Creating a new database $DBNAME.."
-    /usr/bin/psql -U pggis -h localhost -c "CREATE DATABASE $DBNAME WITH OWNER = pggis     ENCODING = 'UTF8'     TEMPLATE = template0    CONNECTION LIMIT = -1;" postgres
-    /usr/bin/psql -U pggis -h localhost -w -c "CREATE EXTENSION postgis; CREATE EXTENSION postgis_topology; CREATE EXTENSION postgis_sfcgal; CREATE EXTENSION pgrouting; CREATE EXTENSION pointcloud; CREATE EXTENSION pointcloud_postgis; drop type if exists texture; create type texture as (url text,uv float[][]);" $DBNAME
-#    /usr/bin/psql -U pggis -h localhost -w -f /usr/share/postgresql/9.5/contrib/postgis-2.1/sfcgal.sql -d $DBNAME
-    echo "Restoring database $DBNAME.."
-    /usr/bin/psql -U pggis -h localhost -d $DBNAME -w -f "$f"
-    echo "Restore done."
-done
+## == Auto restore SQL backups ==
+##
+## If we find some postgresql sql scripts /data/restore, then we load it
+## in new databases
+#shopt -s nullglob
+#for f in /data/restore/*.sql
+#do
+#	echo "Found database SQL dump to restore : $f"
+#    DBNAME=$(basename -s ".sql" "$f")
+#    echo "Creating a new database $DBNAME.."
+#    /usr/bin/psql -U pggis -h localhost -c "CREATE DATABASE $DBNAME WITH OWNER = pggis     ENCODING = 'UTF8'     TEMPLATE = template0    CONNECTION LIMIT = -1;" postgres
+#    /usr/bin/psql -U pggis -h localhost -w -c "CREATE EXTENSION postgis; CREATE EXTENSION postgis_topology; CREATE EXTENSION postgis_sfcgal; CREATE EXTENSION pgrouting; CREATE EXTENSION pointcloud; CREATE EXTENSION pointcloud_postgis; drop type if exists texture; create type texture as (url text,uv float[][]);" $DBNAME
+##    /usr/bin/psql -U pggis -h localhost -w -f /usr/share/postgresql/9.5/contrib/postgis-2.1/sfcgal.sql -d $DBNAME
+#    echo "Restoring database $DBNAME.."
+#    /usr/bin/psql -U pggis -h localhost -d $DBNAME -w -f "$f"
+#    echo "Restore done."
+#done
 
 # == create new database pggis ==
 echo "Creating a new empty database..."
